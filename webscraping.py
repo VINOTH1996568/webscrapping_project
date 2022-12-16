7e0f2a6b852035e925a4631c0154aadbe30fbf70
VINOTH1996568 <119111879+VINOTH1996568@users.noreply.github.com>
2022-12-16 16:46:36 +0530

    Add files via upload

---
+++ webscraping.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import requests, openpyxl
+excel=openpyxl.Workbook()
+sheet=excel.active
+sheet.title="Movie List"
+sheet.append(['rank','movie_name','rate','year'])
+
+try:
+    response=requests.get("https://www.imdb.com/chart/top/")
+    soup=BeautifulSoup(response.text,'html.parser')
+    #print(soup)
+    movies=soup.find("tbody",class_="lister-list").find_all("tr")
+    for movie in movies:
+        rank = movie.find("td",class_="titleColumn").get_text(strip=True).split('.')[0]
+
+
+        movie_name=movie.find("td",class_="titleColumn").a.text
+        rate=movie.find("td",class_="ratingColumn").strong.text
+
+        year = movie.find("td",class_="titleColumn").span.text.replace('(',"")
+        year = year.replace(')','')
+        print(rank,movie_name,rate,year)
+        sheet.append([rank, movie_name, rate, year])
+
+
+except Exception as e:
+    print(e)
+excel.save("Movies.xlsx")
